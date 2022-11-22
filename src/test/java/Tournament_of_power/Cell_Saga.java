0b65e546286b2abb951a68af71114ec84e0edc5a
DELL <DELL@Vivek>
2022-11-22 15:30:47 +0530

    uploaded

---
+++ src/test/java/Tournament_of_power/Cell_Saga.java
@@ -0,0 +1,13 @@
+package Tournament_of_power;
+
+public class Cell_Saga {
+	public static void main(String[] args) {
+		System.out.println("Goku");
+		System.out.println("Broly");
+		System.out.println("Gohan");
+		System.out.println("Vegeta");
+		System.out.println("Trunks");
+		System.out.println("Cumber");
+	}
+
+}
